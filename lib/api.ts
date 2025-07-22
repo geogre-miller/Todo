b8fff55965c4f7001ddc4eb905a8b8213f905dda
geogre-miller <quanghuy00433@gmail.com>
2025-07-22 23:41:05 +0700

    updated version

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { Todo, TodoFormData, TodoResponse } from '@/types/todo';
 
-const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? '/api'  
-  : 'http://localhost:5000/api'; 
+const API_BASE_URL = process.env.NODE_ENV === 'production' 
+  ? 'http://localhost:5000/api'
+  : 'api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
